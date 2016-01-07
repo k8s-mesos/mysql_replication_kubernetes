e4fcab9dc0b47bef336ac3727628e80c78af35ed
zhpooer <zhpooer@gmail.com>
2016-01-07 15:42:27 +0800

    adj debian to ubuntu

--- mysql_async_replication/image/slave/docker-entrypoint.sh
+++ mysql_async_replication/image/slave/docker-entrypoint.sh
@@ -8,7 +8,7 @@ fi
 if [ "$1" = 'mysqld' ]; then
 	# read DATADIR from the MySQL config
 	DATADIR="$("$@" --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
-	
+
 	if [ ! -d "$DATADIR/mysql" ]; then
 		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
 			echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
@@ -25,11 +25,11 @@ if [ "$1" = 'mysqld' ]; then
                 echo 'Initializing database'
                 mysqld --initialize-insecure=on --datadir="$DATADIR"
                 echo 'Database initialized'
-		
+
 		# These statements _must_ be on individual lines, and _must_ end with
 		# semicolons (no line breaks or comments are permitted).
 		# TODO proper SQL escaping on ALL the things D:
-		
+
 		tempSqlFile='/tmp/mysql-first-time.sql'
 		cat > "$tempSqlFile" <<-EOSQL
 			DELETE FROM mysql.user ;
@@ -37,19 +37,19 @@ if [ "$1" = 'mysqld' ]; then
 			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
 			DROP DATABASE IF EXISTS test ;
 		EOSQL
-		
+
 		if [ "$MYSQL_DATABASE" ]; then
 			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
 		fi
-		
+
 		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
 			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
-			
+
 			if [ "$MYSQL_DATABASE" ]; then
 				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
 			fi
 		fi
-		
+
 		echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
 		if [ ! -z "$MYSQL_MASTER_SERVICE_HOST" ]; then
                 	echo "STOP SLAVE;" >> "$tempSqlFile"
@@ -57,10 +57,10 @@ if [ "$1" = 'mysqld' ]; then
                   	echo "START SLAVE;" >> "$tempSqlFile"
 		fi
 
-		
+
 		set -- "$@" --init-file="$tempSqlFile"
 	fi
-	
+
 	chown -R mysql:mysql "$DATADIR"
 fi
 
